8f70523d09d23cbf81bf53ed48962848da4d34df
timwlee <timothyl@adobe.com>
2024-04-21 19:08:45 -0700

    Update cards block

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,14 +1,22 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
-  /* change to ul, li */
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
+    const link = row.querySelector('a');
+    const href = link?.href;
     const li = document.createElement('li');
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
       else div.className = 'cards-card-body';
+
+      if (div.classList.contains('cards-card-image')) {
+        const aTag = document.createElement('a');
+        aTag.href = href ? href : '#';
+        aTag.append(...div.childNodes);
+        div.append(aTag);
+      }
     });
     ul.append(li);
   });
